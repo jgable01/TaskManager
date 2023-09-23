9bc3a32e95b7670a1c7d77351af849b42a6a6c48
jgable01 <126098357+jgable01@users.noreply.github.com>
2023-09-22 22:35:31 -0500

    Added fullname to registration form

--- TaskManager/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TaskManager/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,6 +82,12 @@ namespace TaskManager.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+            [Required]
+            [Display(Name = "Full Name")]
+            [DataType(DataType.Text)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            public string FullName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -119,6 +125,7 @@ namespace TaskManager.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.FullName = Input.FullName.Split(' ').Select(s => s.First().ToString().ToUpper() + s.Substring(1)).Aggregate((s1, s2) => s1 + " " + s2); // Capitalize each word
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
